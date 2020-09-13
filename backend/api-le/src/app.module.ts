7e9d3653af7a1daf83a906d83f45a640d4f185d9
Jeferson Gonçalves de Freitas <freitas000jeferson@gmail.com>
2020-09-13 10:42:39 -0300

    conexao com atlas

--- backend/api-le/src/app.module.ts
+++ backend/api-le/src/app.module.ts
@@ -6,12 +6,19 @@ import { PostagemModule } from './modules/postagem/postagem.module';
 import { UsuarioModule } from './modules/usuario/usuario.module';
 import { MemoryCardModule } from './modules/memory-card/memory-card.module';
 import { TipoPerfilModule } from './modules/tipo-perfil/tipo-perfil.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
-//Cria o link do BD e bota dentro dos imports, ex: 
-//imports: [MongooseModule.forRoot('mongodb+srv://rn_admin:45Y5HJzGLTBGtKB@cluster0.jefip.mongodb.net/dbrn?retryWrites=true&w=majority'), EstagioModule, ProcessoModule],
 @Module({
-
-  imports: [ComentarioModule, PostagemModule, UsuarioModule, MemoryCardModule, TipoPerfilModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://LearningEnglishApiDBUser:l34rn1ng3ngl1sh@learningenglishapi.7mzw1.mongodb.net/LearningEnglishApiDB?retryWrites=true&w=majority',
+    ),
+    ComentarioModule,
+    PostagemModule,
+    UsuarioModule,
+    MemoryCardModule,
+    TipoPerfilModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
